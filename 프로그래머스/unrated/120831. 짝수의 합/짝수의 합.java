5f52a6716b9de7155ae8d5d9038959629e7dd758
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2023-10-18 17:54:04 +0900

    [level unrated] Title: 짝수의 합, Time: 0.01 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(n%2 != 0) n -=1;
+        
+        if(n == 2) return n;
+        if(n == 6) return 2*n;
+        
+        if(n%4 == 0){
+           answer = (n+2)*(n/4);
+        }else{
+           answer = (n+2)/2*(n/2);
+        }
+
+        return answer;
+    }
+}
