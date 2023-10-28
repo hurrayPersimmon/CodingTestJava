e75ef061b5ca6581cf2be2456e62c59ce872bd62
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2023-10-28 23:12:10 +0900

    [level unrated] Title: 정수 부분, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181850. 정수 부분/정수 부분.java
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(double flo) {
-        int answer = (int)(flo - flo % 1);
+        // int answer = (int)(flo - flo % 1);
+        int answer = (int)flo;
         return answer;
     }
 }
