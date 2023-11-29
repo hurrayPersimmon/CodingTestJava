a54cf492e28191b04abda94279dc729c6d39b190
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2023-11-29 21:20:28 +0900

    [level unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int stringToInt = Integer.parseInt(n_str);
+        answer = Integer.toString(stringToInt);
+        return answer;
+    }
+}
