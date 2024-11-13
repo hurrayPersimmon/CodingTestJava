9b304002060a2eafbab038c97e1b04e849f97023
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2024-11-14 06:15:12 +0900

    [level 1] Title: 폰켓몬, Time: 5.45 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Set<Integer> poketmons = new HashSet<Integer>();
+        
+        for(int num : nums) poketmons.add(num); 
+        return answer = poketmons.size() > nums.length / 2? nums.length/2 : poketmons.size();
+    }
+}
