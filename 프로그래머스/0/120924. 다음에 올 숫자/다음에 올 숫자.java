3ffb685c7d461783c879010e7123482172efa091
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2026-03-17 13:18:02 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int[] common) {
+        return common[1] - common[0] == common[2] - common[1]?
+            common[0]+ (common[1]-common[0]) * common.length
+        :   common[1]/common[0] * common[common.length-1];
+    }
+}
