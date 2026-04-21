198e9fced21159c1cc9521be514dd40bd9e49df9
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2026-04-21 11:27:29 +0900

    [level 2] Title: 올바른 괄호, Time: 51.37 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,33 @@
+import java.util.*;
 class Solution {
     boolean solution(String s) {
-        boolean answer = false;
-        int index = 0;
-        int result = 0;
-        
-        for(char x : s.toCharArray()){
-            if(x == '(') result++;
-            else result--;
-            
-            index++;
-            
-            if(result < 0 ||
-               index == s.length() && x == '(')
-                return false;
-              
+        Deque<String> deque = new ArrayDeque<>();
+        for(String x : s.split("")){
+            if(x.equals("(")) deque.push("(");
+            else{
+                if(deque.isEmpty()) return false;
+                deque.pop();
+            }   
         }
+        
+        return deque.isEmpty()? true : false;
+        
+//         boolean answer = false;
+//         int index = 0;
+//         int result = 0;
+        
+//         for(char x : s.toCharArray()){
+//             if(x == '(') result++;
+//             else result--;
+            
+//             index++;
+            
+//             if(result < 0 ||
+//                index == s.length() && x == '(')
+//                 return false;
+              
+//         }
 
-        return answer = result == 0 ? true : false;
+//         return answer = result == 0 ? true : false;
     }
 }
