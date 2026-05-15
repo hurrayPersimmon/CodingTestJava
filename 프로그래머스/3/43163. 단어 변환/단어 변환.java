689531a0c90a396ef9ace7c01805460198b3dba3
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2026-05-15 11:33:08 +0900

    [level 3] Title: 단어 변환, Time: 0.14 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,43 +2,46 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
+        int n = target.length();
         Deque<int[]> queue = new ArrayDeque<>();
-        boolean visited[] = new boolean [words.length];
-        
-        for(int i = 0; i<words.length; i++){
-            if(isChangeable(begin, words[i])) {
+        boolean[] visited = new boolean[words.length];
+        for(int i =0; i<words.length; i++){
+            if(isChangeable(begin, words[i])){
                 queue.offer(new int[]{i,1});
                 visited[i] = true;
-            }  
+            }
         }
         
         while(!queue.isEmpty()){
-            int current[] = queue.poll();
-            int cur = current[0];
-            int distance = current[1];
+            int[] cur = queue.poll();
+            int index = cur[0];
+            int distance = cur[1];
             
-            if(words[cur].equals(target)) return distance;
+            if(target.equals(words[index]))return distance;
             
             for(int i = 0; i<words.length; i++){
-                if(visited[i]) continue;
-                if(cur == i) continue;
-                if(isChangeable(words[cur], words[i])){
-                    queue.offer(new int[]{i, distance +1});
-                    visited[i] = true;
-                } 
+                if(visited[i])continue;
+                if(!isChangeable(words[i], words[index])) continue;
+                queue.offer(new int[]{i,distance+1});
+                visited[i] = true;
+                
             }
         }
+        
+        
         return 0;
     }
     
-    public boolean isChangeable(String begin, String next){
+    public boolean isChangeable(String target, String string){
+        int n = target.length();
         int count = 0;
-        
-        for(int i = 0; i<begin.length(); i++){
-            if(begin.charAt(i) == next.charAt(i)) count++;
+        for(int i = 0; i<n; i++){
+            if(target.charAt(i) == string.charAt(i)) count++;
         }
         
-        return begin.length() - count == 1;
+        return (n - count) == 1;
+        
         
     }
+    
 }
