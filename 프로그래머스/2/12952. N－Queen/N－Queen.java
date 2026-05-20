558fa26010c8934040184fc34d25d1cb51f720bc
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2026-05-20 13:11:46 +0900

    [level 2] Title: N-Queen, Time: 1121.75 ms, Memory: 362 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,54 +1,68 @@
 class Solution {
-    int answer = 0;
-    int queenAmount = 0;
-    int length = 0;
+    int answer, queenAmount, length;
+    
     public int solution(int n) {
+        
         length = n;
-        for(int i = 0; i<n; i++){
-            int[][] queens = new int[n][2];
-            queens[0][0] = 0;
-            queens[0][1] = i;
+        answer = 0;        
+        int queens[][] = new int [n][2];
+
+        for(int i = 0; i<length; i++){
+            queens[0][0] = i;
+            queens[0][1] = 0;
             queenAmount = 1;
             dfs(queens);
+            
         }
+                    
+        
         return answer;
     }
     
     public void dfs(int[][] queens){
+        
         if(queenAmount == length){
             answer++;
             return;
         }
         
-        for(int j = 0; j<length; j++){
-            if(isQueenPossible(queenAmount,j,queens)){
-                queens[queenAmount][0] = queenAmount;
-                queens[queenAmount][1] = j;
+        for(int i = 0; i<length; i++){
+            int x = i;
+            int y = queenAmount;
+            
+            if(isQueenPossible(x,y,queenAmount,queens)){
+                queens[queenAmount][0] = x;
+                queens[queenAmount][1] = y;
                 queenAmount++;
                 dfs(queens);
                 queenAmount--;
             }
         }
+        
     }
     
-    public boolean isQueenPossible(int x, int y, int[][] queens){
+    public boolean isQueenPossible(int x, int y, int queenAmount, int[][]queens){
         for(int i = 0; i<queenAmount; i++){
-            if(queens[i][0] == x || queens[i][1] == y) return false;
+            if(queens[i][0] == x) return false;
+            if(queens[i][1] == y) return false;
             
-            int[] dx = {-1,-1,1,1};
-            int[] dy = {1,-1,-1,1};
+            int dx[] = {-1,1,1,-1};
+            int dy[] = {1,-1,1,-1};
+            
             for(int j = 0; j<4; j++){
-                int nx = x+ dx[j];
-                int ny = y+ dy[j];
+                int nx = queens[i][0];
+                int ny = queens[i][1];
                 
-                while(nx >= 0 && ny >= 0 && nx < length && ny < length){
-                    if(nx == queens[i][0] && ny == queens[i][1]) return false;
-                    nx += dx[j];
-                    ny += dy[j];
+                while(nx>=0 && ny >=0 && nx < length && ny < length){
+                    nx+= dx[j];
+                    ny+= dy[j];
+                    
+                    if(x == nx && y == ny) return false;
                 }
-            }  
+            }
         }
         
         return true;
+        
     }
 }
