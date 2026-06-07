79c3ed882cc817b4c1e96195bf6b5acffc59a7c5
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2026-06-07 15:04:41 +0900

    [level 3] Title: 숫자 게임, Time: 41.18 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -4,9 +4,22 @@ class Solution {
         Arrays.sort(A);
         Arrays.sort(B);
         int index = 0;
-        for (int b : B) {
-            if (b > A[index]) index++;
-            if (index == A.length) break;
+        int length = A.length-1;
+        for(int num : B){
+            int left = index;
+            int right = length;
+            int mid = left + (right-left)/2;
+            
+            while(A[mid] >= num){
+                right = mid;
+                mid = left + (right-left)/2;
+                if(mid <= left)break;
+            }
+            
+            if(A[mid] != 0 && A[mid] < num){
+                A[index] = 0;
+                index++;
+            }
         }
         return index;
     }
