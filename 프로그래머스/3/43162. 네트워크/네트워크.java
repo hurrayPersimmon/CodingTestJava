7e6d0a763a24a24ebaeaa387c4bc5cdf38f0dc59
김민서 <44363238+hurrayPersimmon@users.noreply.github.com>
2026-09-04 00:05:18 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -4,33 +4,28 @@ class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
         Deque<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean [n];
+        boolean[] visited = new boolean[n];
         
-        for(int i = 0; i< n; i++){
+        for(int i=0; i<n; i++){
             if(visited[i]) continue;
-            
             queue.offer(i);
             visited[i] = true;
             
             while(!queue.isEmpty()){
-                int cur = queue.poll();
+                int current = queue.poll();
                 
                 for(int j = 0; j<n; j++){
-                    if(j == cur) continue;
+                    if(j == current) continue;
                     if(visited[j]) continue;
-                    if(computers[cur][j] == 1) {
+                    if(computers[current][j] == 1){
                         queue.offer(j);
                         visited[j] = true;
                     }
                 }
-                
             }
             
             answer++;
-            
-            
         }
-        
         return answer;
     }
 }
